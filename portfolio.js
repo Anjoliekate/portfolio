5fc8f00632e7938cb6afc0407acd570dff18dda8
Anjoliekate <anjoliekatecummins@gmail.com>
2023-02-28 00:51:54 -0700

    adds content to the page including about me

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,21 @@
-setTextOfId(text, id){document.getElementById(id). innerHTML = text;} 
+//setTextOfId(text, id){document.getElementById(id). innerHTML = text;}
+$(function () {
+  // $(document).ready shorthand
+  $(".monster").fadeIn("slow");
+});
+
+$(document).ready(function () {
+  /* Every time the window is scrolled ... */
+  $(window).scroll(function () {
+    /* Check the location of each desired element */
+    $(".hideme").each(function (i) {
+      var bottom_of_object = $(this).position().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      /* If the object is completely visible in the window, fade it it */
+      if (bottom_of_window > bottom_of_object) {
+        $(this).animate({ opacity: "1" }, 1500);
+      }
+    });
+  });
+});
